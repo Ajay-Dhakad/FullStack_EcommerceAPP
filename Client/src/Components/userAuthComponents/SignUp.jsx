65f9d24681bc9eea2c0cb41bbc315dd3e348a0a3
Ajay-Dhakad <ajaydhakad.py@gmail.com>
2024-04-01 18:26:43 +0530

    bug fixed!

--- Client/src/Components/userAuthComponents/SignUp.jsx
+++ Client/src/Components/userAuthComponents/SignUp.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useEffect} from 'react'
 import {Link,useNavigate} from 'react-router-dom'
 import { useState } from 'react';
 import { UseRegister } from './AuthHandlers/authHandlers';
